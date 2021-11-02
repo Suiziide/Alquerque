c3dcfad563436206d63c8bce41a1a176c600fd22
Mikkel <mikkelbn24@live.dk>
2021-11-02 17:17:03 +0100

    Changed the structure of option two to a switch case so that it can give feedback to the user when given an incorrect input. Changed the structure of option 3 to indicate that you have picked option 3 and that two CPUs will play against each other. Also fixed some other random errors in regard to the option menu's layout.

--- src/Alquerque.java
+++ src/Alquerque.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Alquerque {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        String playerOneName = "NoName";
+        String playerOneName = "NoName"; // placeholder names for player 1 and player 2.
         String playerTwoName = "NoName";
         boolean isOneCPU;
         boolean isTwoCPU;
@@ -15,7 +15,7 @@ public class Alquerque {
             option = reader.nextInt();
             switch (option) {
                 case 0:
-                    System.out.println("You have chosen option" + option + ": Exit program");
+                    System.out.println("You have chosen option " + option + ": Exit program");
                     System.out.println("Thank you for playing, have a nice day!");
                     break;
                 case 1: // Player vs Player
@@ -33,25 +33,31 @@ public class Alquerque {
                     do {
                         System.out.print("Please enter the color you want to play black or white (B/W): ");
                         color = reader.next().charAt(0);
+                        switch (color) {
+                            case 'B':
+                            System.out.println("\nYou have chosen to play black the CPU will therefore play white");
+                            System.out.print("Please enter the name of the player: ");
+                            reader.nextLine(); // clears input
+                            playerTwoName = reader.nextLine().trim();
+                            isOneCPU = true;
+                            playerOneName = "CPU";
+                            break;
+                            case 'W':
+                            System.out.println("\nYou have chosen to play white the CPU will therefore play black");
+                            System.out.print("Please enter the name of the player: ");
+                            reader.nextLine(); // clears input
+                            playerOneName = reader.nextLine().trim();
+                            isTwoCPU = true;
+                            playerTwoName = "CPU";
+                            break;
+                            default:
+                                System.out.println("'" + color + "'" + " is not a valid input option, please try again.");
+                        }
                     } while(color != 'B' && color != 'W');
-                    if (color == 'B') {
-                        System.out.println("\n You have chosen to play black the CPU will therefore play white");
-                        System.out.print("Please enter the name of the player: ");
-                        reader.nextLine(); // clears input
-                        playerTwoName = reader.nextLine().trim();
-                        isOneCPU = true;
-                        playerOneName = "CPU";
-                    } else {
-                        System.out.println("\n You have chosen to play white the CPU will therefore play black");
-                        System.out.print("Please enter the name of the player: ");
-                        reader.nextLine(); // clears input
-                        playerOneName = reader.nextLine().trim();
-                        isTwoCPU = true;
-                        playerTwoName = "CPU";
-                    }
                     startGame = true;
                     break;
                 case 3: // CPU vs CPU
+                    System.out.println("You have chosen option " + option + ": CPU vs CPU");
                     isOneCPU = true;
                     isTwoCPU = true;
                     startGame = true;
@@ -78,7 +84,8 @@ public class Alquerque {
      * Prints the option menu to the terminal.
      */
     public static void printOptions() {
-        System.out.println("What do you wish to do?");
+        System.out.println();
+        System.out.println("Now, what do you wish to do?");
         System.out.println();
         System.out.println("Option 0: Exit program");
         System.out.println("Option 1: Player vs Player");

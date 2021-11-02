2b4875c155fce5c1734698f0e1ec8ee7319cf771
suiziide <nicolai9595@hotmail.com>
2021-11-02 13:56:05 +0100

    user variables?

--- src/Alquerque.java
+++ src/Alquerque.java
@@ -1,5 +1,9 @@
 public class Alquerque {
     public static void main(String[] args) {
+        String playerOneName;
+        String playerTwoName;
+        boolean isOneAi;
+        boolean isTwoAi;
 
 
 

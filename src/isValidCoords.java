275220c366e87f2718fc25eddf2ec7fa936da7e1
suiziide <nicolai9595@hotmail.com>
2021-11-03 21:05:33 +0100

    Changed matching by charat and ascii to matching by regex - Much better :D

--- src/isValidCoords.java
+++ src/isValidCoords.java
@@ -4,7 +4,6 @@ public class isValidCoords {
     * a letter from A-E followed by a number from 1 - 5
     */
     private static boolean isValidCoords(String coords){
-        return (coords.length() == 2 && coords.charAt(0) >= 65 && coords.charAt(0) <= 69 &&
-                coords.charAt(1) >= 49 && coords.charAt(1) <= 53); // ASCII (65 = A, 69 = E, 49 = 1, 53 = 5)
+        return (coords.matches("[A-E][1-5]")); // Regex for matching
     }
 }

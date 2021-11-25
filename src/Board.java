a7eb4eba0b3ce78187fcd860dd9a72c6da36193f
MikkelBN <mikkelbn24@live.dk>
2021-11-25 09:01:00 +0100

    Changed isTakeMove

--- src/Board.java
+++ src/Board.java
@@ -86,11 +86,10 @@ public class Board {
     /*
      * checks whether the move is a take move
      */
-    private boolean isTakeMove(Move move){
-        //boolean isWhite = (turn % 2 == 1); //checks turn
+    private boolean isTakeMove(Move move) {
         return ((Math.abs(move.to() - move.from()) > 6 || Math.abs(move.to() - move.from()) < 4) && 
-                (isWhite && board[(move.to() + move.from()) / 2] != 'B') && //checks if opponent piece is taken
-                (!isWhite && board[(move.to() + move.from()) / 2] != 'W')); //checks if opponent piece is taken
+                (isWhite && board[(move.to() + move.from()) / 2] == 'B') && //checks if opponent piece is taken
+                (!isWhite && board[(move.to() + move.from()) / 2] == 'W')); //checks if opponent piece is taken
         //ought to be revisited - might be better written as if-statements
     }
 

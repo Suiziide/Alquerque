53480706669463e1d1f232e489a362d562bb3034
suiziide <nicolai9595@hotmail.com>
2021-11-25 15:15:23 +0100

    Created isValid + fileDiff + pieceDiff methods

--- src/Board.java
+++ src/Board.java
@@ -12,15 +12,6 @@ public class Board {
     public Board() {
         turn = 1;
         board = new char[26];
-        for (int i = 0; i < 26; i++) {
-            if (i != 15 && i != 4) {
-                board[i] = ' ';
-            } else {
-                board[4] = 'B';
-                board[15] = 'W';
-            }
-        }
-        /*
         for (int i = 1; i < 26; i++) {
             if (i < 13)
                 board[i] = 'B';
@@ -29,11 +20,12 @@ public class Board {
             else
                 board[i] = 'W';
         }
-      */
         isWhite = (turn % 2 == 1);
     }
+
     /**
      * Returns the positions of all black pieces on the board.
+     *
      * @return the positions of all black pieces on the board.
      */
     public int[] black() {
@@ -49,6 +41,7 @@ public class Board {
 
     /**
      * Returns the positions of all white pieces on the board.
+     *
      * @return the positions of all white pieces on the board.
      */
     public int[] white() {
@@ -66,6 +59,7 @@ public class Board {
     /**
      * Moves a piece and updates the board correspondingly.
      * Precondition: move must be a valid Move
+     *
      * @param move the move to simulate.
      */
     public void move(Move move) {
@@ -81,40 +75,60 @@ public class Board {
     /**
      * Checks whether a move is legal.
      * Precondition: move must be an int from 1 through 25
+     *
      * @param move move input to evaluate.
      */
-    public boolean isLegal(Move move){
+    public boolean isLegal(Move move) {
         if (board[move.to()] != ' ') {
             System.out.println("Not empty");
             return false;
-        }
-        else if ((isWhite && board[move.from()] != 'W') || (!isWhite && board[move.from()] != 'B')) {
+        } else if ((isWhite && board[move.from()] != 'W') || (!isWhite && board[move.from()] != 'B')) {
             System.out.println("Wrong piece");
             return false;
-            //  } else if (!isTakeMove(move)) {
-            //     return false;
-        } else if (move.from() % 5 == 0 && ((move.to() - 1) % 5 < 4)) {
-            System.out.println("Not a take move");
+        } else if (fileDiff(move) > 2) {
+            System.out.println("Trying to move to an illegal column");
             return false;
-        } else {
-            System.out.println("From: " + move.from());
-            System.out.println("To: " + move.to());
-            System.out.println("isWhite: " + isWhite);
-            return true;
+        } else if (!isTakeMove(move)) {
+            if ((isWhite && (pieceDiff(move) < -6 || pieceDiff(move) > -4)) ||
+                    (!isWhite && (pieceDiff(move) < 4 || pieceDiff(move) > 6)))
+                return false;
+            else if (move.from() % 2 == 0 && move.to() % 2 == 0)
+                return false;
+        } else if (isTakeMove(move)) {
+            if (Math.abs(pieceDiff(move)) != 2 && Math.abs(pieceDiff(move)) != 8 &&
+                    Math.abs(pieceDiff(move)) != 10 && Math.abs(pieceDiff(move)) != 12) {
+                return false;
+            }else if (move.from() % 2 == 0 && move.to() % 2 == 1)
+                return false;
+            else if (move.from() % 2 == 0 && Math.abs(pieceDiff(move)) != 10 && Math.abs(pieceDiff(move)) != 2)
+                return false;
         }
+        System.out.println("From: " + move.from());
+        System.out.println("To: " + move.to());
+        System.out.println("isWhite: " + isWhite);
+        return true;
+    }
+
+
+    /*
+     * Auxillerary methods to check how far there are between columns columns
+     */
+    private int fileDiff(Move move){
+        return Math.abs(((move.from() - 1) % 5 + 1) - ((move.to() - 1) % 5 + 1));
+    }
+    /*
+     * Auxillerary method to check how far there is between two pieces
+     */
+    private int pieceDiff(Move move) {
+        return (move.to() - move.from());
     }
     /*
      * checks whether the move is a take move
      */
     private boolean isTakeMove(Move move) {
-        return ((Math.abs(move.to() - move.from()) > 6 || Math.abs(move.to() - move.from()) < 4) && (
+        return ((Math.abs(pieceDiff(move)) > 6 || Math.abs(pieceDiff(move)) < 4) && (
                 (isWhite && board[(move.to() + move.from()) / 2] == 'B') || //checks if opponent piece is taken
                         (!isWhite && board[(move.to() + move.from()) / 2] == 'W'))); //checks if opponent piece is taken
         //ought to be revisited - might be better written as if-statements
     }
-
-
-
-
-
 }

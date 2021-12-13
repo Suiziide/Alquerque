17567a0b0ccf08df44c92af91e23bbcfecc76820
MikkelBN <mikkelbn24@live.dk>
2021-12-13 12:39:14 +0100

    Moved some things around in preparation to start the 3rd phase.

---
+++ del1 completed/project1/Alquerque.java
@@ -0,0 +1,251 @@
+import java.util.Scanner;
+public class Alquerque {
+    private static Scanner reader;
+    private static Board board;
+    private static final char EMPTY = ' ';
+    private static String whiteName, blackName;
+    private static int cpuDepth;
+    private static boolean isWhiteCPU, isBlackCPU, isWhite;
+
+    public static void main(String[] args) {
+        String coordsFrom;
+        String coordsTo;
+        Move nextMove = new Move(0,0);
+        init();
+        do { // main game loop
+            printBoard();
+            if (!isWhiteCPU && isWhite || !isBlackCPU && !isWhite) {
+                boolean inputWithinRange = false;
+                do { // loop for validating the players input
+                    System.out.print("It's " + (isWhite ? whiteName : blackName) + "'s turn" + ", please enter which " +
+                            "piece you want to move: ");
+                    coordsFrom = reader.nextLine().trim();
+                    System.out.print("Please enter where you want to move the piece: ");
+                    coordsTo = reader.nextLine().trim();
+                    if (isValidCoords(coordsFrom) && isValidCoords(coordsTo)) { //Checks if input is a valid letter+number
+                        nextMove = new Move(convertCoordinate(coordsFrom), convertCoordinate(coordsTo)); //Converts coordinate to int position
+                        if (board.isLegal(nextMove))
+                            inputWithinRange = true;
+                    }
+                    if (!inputWithinRange)
+                        System.out.println(coordsFrom + " to " + coordsTo + " is " +
+                                "not a valid move, please enter a coordinate A-E 1-5.");
+                } while (!inputWithinRange);
+                board.move(nextMove);
+            } else if (!board.isGameOver()) {
+                nextMove = Minimax.nextMove(board, cpuDepth, isWhite);
+                System.out.println((isWhite ? whiteName : blackName) + " played " +
+                        convertPosition(nextMove.from()) + " to " + convertPosition(nextMove.to()));
+                board.move(nextMove);
+            }
+            isWhite = !isWhite; // changes who's turn it is at the end of a turn
+        } while (!board.isGameOver());
+        System.out.println("This is the final state of the board");
+        printBoard(); // prints the state of the board when game over
+        if (board.black().length > 0 && board.white().length <= 0)
+            System.out.println(blackName + " is the winner!");
+        else if (board.black().length <= 0 && board.white().length > 0)
+            System.out.println(whiteName + " is the winner!");
+        else
+            System.out.println("It's a draw!");
+    }
+
+    /**
+     * Initializes the program and runs the start menu.
+     */
+    private static void init() {
+        reader = new Scanner(System.in);
+        board = new Board();
+        whiteName = "White(CPU)";
+        blackName = "Black(CPU)";
+        isWhite = true;
+        int option;
+        System.out.println("*******************************************");
+        System.out.println("Greetings Master! And welcome to Alquerque.");
+        System.out.println("*******************************************");
+        do {
+            printOptions();
+            option = reader.nextInt();
+            switch (option) {
+                case 0:
+                    System.out.println("You have chosen option " + option + ": Exit program");
+                    System.out.println("Thank you for playing, have a nice day!");
+                    break;
+                case 1: // Player vs Player
+                    System.out.println("You have chosen option " + option + ": Player vs Player");
+                    System.out.print("Please enter the name of player 1: ");
+                    reader.nextLine(); // clears input
+                    whiteName = reader.nextLine().trim();
+                    System.out.print("Please enter the name of player 2: ");
+                    blackName = reader.nextLine().trim();
+                    break;
+                case 2: // Player vs CPU
+                    System.out.println("You have chosen option " + option + ": Player vs CPU");
+                    String color;
+                    reader.nextLine(); // clears input
+                    do {
+                        System.out.print("Please enter the color you want to play " +
+                                "black or white (B/W): ");
+                        color = reader.nextLine();
+                        if (color.matches("[Bb]")){
+                            System.out.println("\nYou have chosen to play black.\n" +
+                                    "The CPU will therefore play white");
+                            System.out.print("Please enter the name of the player: ");
+                            blackName = reader.nextLine().trim();
+                            isWhiteCPU = true;
+                        } else if (color.matches("[Ww]")){
+                            System.out.println("\nYou have chosen to play white.\n" +
+                                    "The CPU will therefore play black");
+                            System.out.print("Please enter the name of the player: ");
+                            whiteName = reader.nextLine().trim();
+                            isBlackCPU = true;
+                        } else {
+                            System.out.println("'" + color + "'" + " is not a valid color " +
+                                    "option, please try again.\n");
+                        }
+                    } while (!color.matches("[BbWw]"));
+                    System.out.print("How far ahead do you want the CPU to analyze: ");
+                    cpuDepth = reader.nextInt();
+                    reader.nextLine(); // clears input
+                    break;
+                case 3: // CPU vs CPU
+                    System.out.println("You have chosen option " + option + ": CPU vs CPU");
+                    System.out.print("How far ahead do you want the CPU's to analyze: ");
+                    cpuDepth = reader.nextInt();
+                    isWhiteCPU = true;
+                    isBlackCPU = true;
+                    break;
+                default:
+                    System.out.println("Invalid option, " + option + " is not a valid option.\n");
+            }
+        } while (option > 3 || option < 0);
+    }
+
+    /**
+     * Prints the option menu to the terminal.
+     */
+    private static void printOptions() {
+        System.out.println("Now, what do you wish to do?");
+        System.out.println("****************************");
+        System.out.println("Option 0: Exit program");
+        System.out.println("Option 1: Player vs Player");
+        System.out.println("Option 2: Player vs CPU");
+        System.out.println("Option 3: CPU vs CPU");
+        System.out.println("****************************");
+        System.out.println();
+        System.out.print("Please enter the number corresponding " +
+                "to the option you want executed: ");
+    }
+
+    /**
+     * Creates a representation of the game board with the pieces correctly placed
+     * in the form of a two dimensional array.
+     * Precondition: Relies on method black() and white() to return valid positions numbered from 1-25
+     * @return a two dimensional array 5 x 5 with the game pieces placed correctly
+     */
+    private static char[][] boardWithPieces() {
+        char[][] boardArr = new char[6][5]; //A-E & (no 0) 1-5
+        for (int j = 1; j < boardArr.length; j++)
+            for (int i = 0; i < boardArr[j].length; i++)
+                boardArr[j][i] = EMPTY; // Fills board with empty spaces
+        for (int i = 0; i < board.black().length; i++)
+            boardArr[((board.black()[i] - 1) / 5) + 1][((board.black()[i] - 1) % 5)] = 'B'; // Places black pieces
+        for (int i = 0; i < board.white().length; i++)
+            boardArr[((board.white()[i] - 1) / 5) + 1][((board.white()[i] - 1) % 5)] = 'W'; // Places white pieces
+        return boardArr;
+    }
+
+    /**
+     * prints a representation of the board to the terminal
+     */
+    private static void printBoard() {
+        System.out.println(); // new line
+        int i = 0, j = 1;
+        System.out.println("   A   B   C   D   E"); //upper-coordinate-line (A-E)
+        char[][] boardWithPieces = boardWithPieces();
+        while (j < 6) {
+            System.out.print(j + " "); //left-hand coordinate (1-5)
+            while (i < 5) {
+                System.out.print("[" + boardWithPieces[j][i] + "]");
+                if (i < 4)
+                    System.out.print("-");
+                i++;
+            }
+            System.out.print(" " + (j)); //right-hand coordinate (1-5)
+            System.out.println("");
+            i = 0;
+            if (j % 2 == 1 && j < 5)
+                System.out.println("   | \\ | / | \\ | / |");
+            else if (j % 2 == 0)
+                System.out.println("   | / | \\ | / | \\ |");
+            j++;
+        }
+        System.out.println("   A   B   C   D   E"); //bottom-coordinate-line (A-E)
+        System.out.println(""); // new line
+    }
+
+    /**
+     * Test whether an enterede coordinate is a valid coordinate
+     * @param coords, a coordinate to be tested
+     * @return true if the coordinate entered is a valid coordinate, returns false if not
+     */
+    private static boolean isValidCoords(String coords){
+        return (coords.matches("[A-Ea-e][1-5]")); // Regex for matching
+    }
+
+    /**
+     * Converts an input coordinate to the corresponding position on the board, determined by numbers 1-25
+     * @param coord move coordinate input from user
+     * @return position on board, represented by an integer (1-25)
+     */
+    private static int convertCoordinate(String coord){
+        int position = 0;
+        switch(Character.toUpperCase(coord.charAt(0))){
+            case 'A':   //value of each column is added to the row-determined multiplum of 5 (e.g. D is 4'th, so positional value is +4)
+                position = (1+(5*((Integer.parseInt(coord.substring(1))-1))));
+                break;
+            case 'B':
+                position = (2+(5*((Integer.parseInt(coord.substring(1))-1))));
+                break;
+            case 'C':
+                position = (3+(5*((Integer.parseInt(coord.substring(1))-1))));
+                break;
+            case 'D':
+                position = (4+(5*((Integer.parseInt(coord.substring(1))-1))));
+                break;
+            case 'E':
+                position = (5+(5*((Integer.parseInt(coord.substring(1))-1))));
+                break;
+            default:
+                return 0;
+        }
+        return position;
+    }
+    /**
+     * Converts an input position, represented by a number 1-25 to the corresponding coordinates in form [A-E][1-5]
+     * @param position position represented by an int
+     * @return coord position represented by coordinates [A-E][1-5]
+     */
+    private static String convertPosition(int position){
+        String coord = "";
+        switch ((position - 1) % 5){
+            case 0:
+                coord = "A";
+                break;
+            case 1:
+                coord = "B";
+                break;
+            case 2:
+                coord = "C";
+                break;
+            case 3:
+                coord = "D";
+                break;
+            case 4:
+                coord = "E";
+                break;
+        }
+        coord = coord + ((position - 1) / 5 + 1);
+        return coord;
+    }
+} // end of alquerque class
---
+++ del2 completed/project2/Board.java
@@ -0,0 +1,208 @@
+import java.util.ArrayList;
+
+public class Board {
+    private char[] board;
+    private int turn;
+    private boolean isWhite;
+    private boolean isGameDone;
+    private static int finishedGames = 0;
+    private static final char EMPTY = ' ';
+
+    /**
+     * Creates a new Alquerque board in the starting state:
+     * each player has twelve pieces in their original position, and it is white's turn.
+     */
+    public Board() {
+        turn = 1;
+        board = new char[26];
+        for (int i = 1; i < 26; i++) {
+            if (i < 13)
+                board[i] = 'B';
+            else if (i == 13)
+                board[i] = EMPTY;
+            else
+                board[i] = 'W';
+        }
+        isWhite = (turn % 2 == 1);
+        isGameDone = false;
+    }
+
+    /**
+     * Returns the positions of all black pieces on the board.
+     *
+     * @return the positions of all black pieces on the board.
+     */
+    public int[] black() {
+        ArrayList<Integer> blackPieces = new ArrayList<Integer>();
+        for (int i = 1; i <= 25; i++)
+            if (this.board[i] == 'B')
+                blackPieces.add(i);
+        int[] black = new int[blackPieces.size()];
+        for (int i = 0; i < blackPieces.size(); i++)
+            black[i] = blackPieces.get(i);
+        return black;
+    }
+
+    /**
+     * Returns the positions of all white pieces on the board.
+     * @return the positions of all white pieces on the board.
+     */
+    public int[] white() {
+        ArrayList<Integer> whitePieces = new ArrayList<Integer>();
+        for (int i = 1; i <= 25; i++)
+            if (this.board[i] == 'W')
+                whitePieces.add(i);
+        int[] white = new int[whitePieces.size()];
+        for (int i = 0; i < whitePieces.size(); i++)
+            white[i] = whitePieces.get(i);
+        return white;
+    }
+
+    /**
+     * Moves a piece and updates the board correspondingly.
+     * Precondition: move must be a legal between 1 and 25
+     * @param move the move to simulate.
+     */
+    public void move(Move move) {
+        board[move.to()] = board[move.from()];
+        board[move.from()] = EMPTY;
+        if (isTakeMove(move))    //if the move is a take, the taken piece is removed
+            board[(move.to() + move.from()) / 2] = EMPTY; //calculates average position value and removes piece
+        // Updates who's turn it is
+        this.turn++;
+        isWhite = (turn % 2 == 1);
+        // updates finishedGames after eachmove
+       if (isGameOver() && !isGameDone) {
+           finishedGames++;
+           isGameDone = true;
+       }
+    }
+
+    /**
+     * Checks whether a move is legal.
+     * Precondition: move must be an int from 1 through 25
+     * @param move move input to evaluate.
+     */
+    public boolean isLegal(Move move) {
+        if (board[move.to()] != EMPTY)  // Checks whether the player tries to move from an empty cell
+            return false;
+        else if ((isWhite && board[move.from()] != 'W') || (!isWhite && board[move.from()] != 'B'))
+            // Checks if the player tries to move the opponents piece
+            return false;
+        else if (fileDiff(move) > 2)
+            // Checks if the player tries to move to a column that is too far away, which prevents moves rolling over from one row to the next
+            return false;
+        else if (!isTakeMove(move)){ // Logic for regular moves
+            if ((isWhite && (pieceDiff(move) < -6 || pieceDiff(move) > -4)) ||
+                    (!isWhite && (pieceDiff(move) < 4 || pieceDiff(move) > 6)))
+                // Checks if direction is correct and if it is within the range of allowed cells to move to
+                return false;
+            else if (move.from() % 2 == 0 && move.to() % 2 == 0)
+                // Check for moves on even cells (To confirm that it follows the lines on the board)
+                return false;
+        } else if (isTakeMove(move)) { // Logic for moves that take another piece
+            if (Math.abs(pieceDiff(move)) != 2 && Math.abs(pieceDiff(move)) != 8 &&
+                    Math.abs(pieceDiff(move)) != 10 && Math.abs(pieceDiff(move)) != 12)
+                // Checks if the move is to the specified allowed cells for a take move
+                return false;
+            else if (move.from() % 2 == 0 && Math.abs(pieceDiff(move)) != 10 && Math.abs(pieceDiff(move)) != 2)
+                // Checks for moves on even cells (to confirm it follows the lines on the board)
+                return false;
+        }
+        return true;
+    }
+
+    /**
+     * Returns an array of all legal moves for this board
+     * @return an array of all legal moves for this board
+     */
+    public Move[] legalMoves() {
+        ArrayList<Move> legalList = new ArrayList<Move>();
+        for (int i = 1; i < board.length; i++)
+            if (board[i] != EMPTY)
+                for (int j = 1; j < board.length; j++)
+                    if (isLegal(new Move(i,j)))
+                        legalList.add(new Move(i,j));
+        Move[] legalMoves = new Move[legalList.size()];
+        for (int i = 0; i < legalList.size(); i++)
+            legalMoves[i] = legalList.get(i);
+        return legalMoves;
+    }
+
+    /**
+     * Returns if the game is over
+     * @return if the game is over
+     */
+    public boolean isGameOver() {
+        return (white().length == 0 || black().length == 0 || legalMoves().length == 0);
+    }
+
+    /**
+     * Returns how many objects of type Board that represents games, that are finished games.
+     * @return how many objects of type Board that represents games, that are finished games.
+     */
+    public static int finishedGames() {
+        return finishedGames;
+    }
+
+    /**
+     * Returns a copy of this board
+     * @return a copy of this board
+     */
+    public Board copy() {
+        Board newBoard = new Board();
+        for (int i = 0; i < this.board.length; i++)
+            newBoard.board[i] = this.board[i];
+        newBoard.turn = this.turn;
+        newBoard.isWhite = this.isWhite;
+        newBoard.isGameDone = this.isGameDone;
+        return newBoard;
+    }
+
+    /**
+     * Checks whether this Board is equal to other Object
+     * @param other Object to check against this board
+     * @return whether this Board is equal to other Object
+     */
+    public boolean equals(Object other){
+        if (other == null) return false;
+        else if (this == other) return true;
+        else if (!(other instanceof Board)) return false;
+        Board otherBoard = (Board) other;
+        int i = 0;
+        while(i < this.board.length && this.board[i] == otherBoard.board[i])
+            i++;
+        return (i == this.board.length && this.turn == otherBoard.turn && this.isGameDone == otherBoard.isGameDone);
+    }
+
+    /**
+     * Returns a hashCode compised of this boards attributes
+     * @return a hashCode comprised of this boards attributes
+     */
+    public int hashCode() {
+        return (this.board.hashCode() + this.turn*31);
+    }
+
+    /*
+     * Auxiliary methods to check how far there are between the columns in the move
+     */
+    private int fileDiff(Move move){
+        return Math.abs(((move.from() - 1) % 5 + 1) - ((move.to() - 1) % 5 + 1));
+    }
+
+    /*
+     * Auxiliary method to check how far there is between two pieces
+     */
+    private int pieceDiff(Move move) {
+        return (move.to() - move.from());
+    }
+
+    /*
+     * checks whether the move is a take move
+     */
+    private boolean isTakeMove(Move move) {
+        return ((Math.abs(pieceDiff(move)) > 6 || Math.abs(pieceDiff(move)) < 4) &&
+                ((isWhite && board[(move.to() + move.from()) / 2] == 'B') || //checks if opponent piece is taken
+                        (!isWhite && board[(move.to() + move.from()) / 2] == 'W'))); //checks if opponent piece is taken
+    }
+}

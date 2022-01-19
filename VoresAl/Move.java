9e93761b60508b04e52123d3614038be1f2778b8
MikkelBN <mikkelbn24@live.dk>
2022-01-19 09:56:02 +0100

    updatede some things

--- VoresAl/Move.java
+++ VoresAl/Move.java
@@ -8,7 +8,7 @@ public class Move {
      */
     public Move(int from, int to) {
         this.from = from;
-        this.to = t o;
+        this.to = to;
     }
 
     /**
@@ -25,5 +25,34 @@ public class Move {
      */
     public int to() {
         return to;
+    }
+
+    /**
+     * Creates a copy of this move
+     * @return a copy of this move
+     */
+    public Move copy() {
+        return new Move(this.from, this.to);
+    }
+
+    /**
+     * Checks whether other move is equal to this move
+     * @param other
+     * @return whether other move is equal to this move
+     */
+    public boolean equals(Object other) {
+        if (other == null) return false;
+        else if (this == other) return true;
+        else if (!(other instanceof Move)) return false;
+        Move otherMove = (Move) other;
+        return (this.from == otherMove.from && this.to == otherMove.to);
+    }
+
+    /**
+     * creates a hashCode for this move
+     * @return the hashCode for this move
+     */
+    public int hashCode() {
+        return (this.from + this.to*31);
     }
 }

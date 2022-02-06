b43eb05e9dacc57282153a61332062e2f99928d6
suiziide <nicolai9595@hotmail.com>
2022-02-06 23:11:26 +0100

    Fixed the stack

--- VoresAl/Minimax.java
+++ VoresAl/Minimax.java
@@ -8,6 +8,6 @@ public class Minimax {
      * @return
      */
     public static Move nextMove(Board board, int depth, boolean isWhite) {
-        return ((new MinimaxTree(board, depth, isWhite)).nextMove());
+        return ((new MinimaxTree(board, depth e, isWhite)).nextMove());
     }
 }
